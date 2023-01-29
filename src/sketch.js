b25760218e0314b56e3d8ed8bc046801f8e46f0c
Anne Lee <annelee@annes-mbp-2.home>
2023-01-29 23:53:58 +0100

    Pixelation loop

--- src/sketch.js
+++ src/sketch.js
@@ -1,46 +1,85 @@
 let myFont
 let graphic
+let tileCount = 40
+let tileColumnSize
+let tileRowSize
 
 function preload() {
   myFont = loadFont('assets/spacegrotesk-medium.otf')
 }
 
 function setup() {
-  createCanvas(900, 900)
+  createCanvas(windowWidth, windowHeight)
   background(0)
+  noStroke()
+  strokeWeight(0)
+  // noCursor()
 
-  graphic = createGraphics(900, 900)
+  graphic = createGraphics(windowWidth, windowHeight)
 
   graphic.fill(255)
   graphic.textAlign(CENTER, CENTER)
-  graphic.textSize(1000)
+  graphic.textSize(360)
   graphic.textFont(myFont)
-  graphic.text("a", 450, 350)
-}
+  graphic.text("GRAPHIC", windowWidth/2, windowHeight/2 - 150)
+  graphic.text("DESIGN", windowWidth/2, windowHeight/2 + 150)
 
-function draw() {
-  background(0)
+  tileColumnSize = windowWidth / tileCount
+  tileRowSize = windowHeight / tileCount
 
-  const tileSize = 100
-
-  for (let i=0; i<12; i++) {
-    for (let j=0; j<12; j++) {
-      const distortion = sin(frameCount*0.05 + i*0.5 + j*0.3)*50
+  for (let i=0; i<tileCount; i++) {
+    for (let j=0; j<tileCount; j++) {
 
       // Source
-      const sx = i*tileSize + distortion
-      const sy = j*tileSize
-      const sw = tileSize
-      const sh = tileSize + distortion
+      const sx = i*tileCount - tileColumnSize / 2
+      const sy = j*tileCount - tileRowSize / 2
+      const sw = tileColumnSize
+      const sh = tileRowSize
 
       // Destination
-      const dx = i*tileSize
-      const dy = j*tileSize
-      const dw = tileSize
-      const dh = tileSize
+      const dx = i*tileCount
+      const dy = j*tileCount
+      const dw = tileColumnSize
+      const dh = tileRowSize
 
 
       image(graphic, dx, dy, dw, dh, sx, sy, sw, sh)
     }
   }
 }
+
+function draw() {
+  background(0)
+  // noCursor()
+  tileCountInit = Math.abs(cos(frameCount * 0.004) * 10)
+  tileCount = map(tileCountInit, 0, 10, 60, 30)
+  console.log(tileCount)
+
+  tileColumnSize = windowWidth / tileCount
+  tileRowSize = windowHeight / tileCount
+
+  for (let i=0; i<tileCount; i++) {
+    for (let j=0; j<tileCount; j++) {
+
+      let color = graphic.get(i * tileColumnSize, j * tileRowSize)
+      let colorVal = color[0]
+
+      noStroke()
+      strokeWeight(0)
+      if (colorVal == 0) {
+        fill(color)
+      } else {
+        fill(255)
+      }
+
+      rect(i*tileColumnSize - tileColumnSize / 2, j*tileRowSize - tileRowSize / 2, tileColumnSize, tileRowSize)
+    }
+  }
+
+  // fill(255, 0, 0)
+  // ellipse(mouseX, mouseY, 50)
+}
+
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight)
+// }
